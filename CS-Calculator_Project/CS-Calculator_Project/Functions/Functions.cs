36d865868d863dcee726fe3e747aa4df7fc70710
Sesmic <Sesmicwastaken@tutamail.com>
2022-03-10 17:12:08 +0000

    initial commit

---
+++ CS-Calculator_Project/CS-Calculator_Project/Functions/Functions.cs
@@ -0,0 +1,34 @@
+﻿namespace CS_Calculator.Functions
+{
+    public class MathOperators
+    {
+        public static double ValueA, ValueB;
+        public static double Operators(string input)
+
+        {
+
+            double output = 0;
+            string Operator = input.Trim();
+            switch (Operator)
+            {
+                case "+":
+                    output = ValueA + ValueB;
+                    break;
+                case "-":
+                    output = ValueA - ValueB;
+                    break;
+                case "/":
+                    output = ValueA / ValueB;
+                    break;
+                case "*":
+                    output = ValueA * ValueB;
+                    break;
+                case "^":
+                    output = Math.Pow(ValueA, ValueB);
+                    break;
+                   
+            }
+            return output;
+        }
+    }
+}
---
+++ CS-Calculator_Project/CS-Calculator_Project/Input/Input.cs
@@ -0,0 +1,59 @@
+﻿using CS_Calculator.Functions;
+
+namespace CS_Calculator.Input
+{
+    public class Input
+    {
+        public static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Write("Enter 1st value:");
+                string valueA = Console.ReadLine().Trim();
+                if (string.IsNullOrEmpty(valueA))
+                {
+                    Console.WriteLine("Value cannot be null.");
+                    return;
+                }
+                MathOperators.ValueA = Convert.ToDouble(valueA);
+
+                Console.Write("Enter the operator:");
+                string input = Console.ReadLine();
+                if (input == null)
+                {
+                    Console.WriteLine("Operator cannot be null");
+                    return;
+                }
+
+                Console.Write("Enter 2nd value:");
+                string valueB = Console.ReadLine().Trim();
+
+                if (string.IsNullOrEmpty(valueB))
+                {
+                    Console.WriteLine("Value cannot be null.");
+                    return;
+                }
+
+                MathOperators.ValueB = Convert.ToDouble(valueB);
+
+                Console.WriteLine(MathOperators.Operators(input));
+
+                Console.WriteLine("Do you want to continue?");
+                string answer = Console.ReadLine().ToLower().Trim();
+                if (answer == "no" || string.IsNullOrEmpty(answer))
+                {
+                    return;
+                }
+                else if (answer == "yes")
+                {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
+
+
+
